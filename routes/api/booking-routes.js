2c70de0f36ec6645ffafe7c210e795bcf08d8f65
El Roble <san_lucas2005@yahoo.com>
2021-08-19 14:00:21 -0600

    Add GET and POST methods to api/booking

---
+++ routes/api/booking-routes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Booking } = require('../../models');
+
+// The `/api/booking` endpoint
+
+router.get('/', async (req, res) => {
+  // find all activities
+  // be sure to include its associated BookingHours
+  try {
+    const bookingData = await Booking.findAll();
+    res.status(200).json(bookingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:Booking_name', async (req, res) => {
+  // find one Booking by its `id` value
+  // be sure to include its associated BookingHours
+  try {
+    const bookingData = await Booking.findOne({ where: { Booking_name: req.params.Booking_name },
+       include: [{ model: BookingHour }], 
+      },
+    );
+
+    if (!bookingData) {
+      res.status(404).json({ message: 'No Booking found with that id!' });
+      return;
+    }
+
+    res.status(200).json(bookingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }  
+});
+
+router.post('/', async (req, res) => {
+  // create a new Booking
+    try {
+      const bookingData = await Booking.create({
+        hour: req.body.hour,
+        date: req.body.date,
+        activity_id: req.body.activity_id,
+      });
+      res.status(200).json(bookingData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// router.put('/:id', async (req, res) => {
+//   // update a Booking by its `id` value
+//   try {
+//     const bookingData = await Booking.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     if (!bookingData[0]) {
+//       res.status(404).json({ message: 'No Booking with this id!' });
+//       return;
+//     }
+//     res.status(200).json(bookingData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const activityRoutes = require('./activity-routes');
 const activityHourRoutes = require('./activity-hour-routes');
+const bookings = require('./booking-routes');
 
 router.use('/activities', activityRoutes);
 router.use('/activitiesHours', activityHourRoutes);
+router.use('/bookings', bookings);
 
 module.exports = router;
